628bd7d559f9b4169ec28ed342d2943aeedf8fc0
Sunil Barik <sunilbarik945@gmail.com>
2023-09-21 18:04:58 +0530

    First Commit

---
+++ CurrencyConvertorApp/script.js
@@ -0,0 +1,55 @@
+// api for currency change
+const api = "https://api.exchangerate-api.com/v4/latest/USD";
+
+
+let search = document.querySelector(".searchBox");
+let convert = document.querySelector(".convert");
+let fromCurrecy = document.querySelector(".from");
+let toCurrecy = document.querySelector(".to");
+let finalValue = document.querySelector(".finalValue");
+let finalAmount = document.getElementById("finalAmount");
+let resultFrom;
+let resultTo;
+let searchValue;
+
+// Event when currency is changed
+fromCurrecy.addEventListener('change', (event) => {
+	resultFrom = `${event.target.value}`;
+});
+
+
+toCurrecy.addEventListener('change', (event) => {
+	resultTo = `${event.target.value}`;
+});
+
+search.addEventListener('input', updateValue);
+
+function updateValue(e) {
+	searchValue = e.target.value;
+}
+
+// When user clicks, it calls function getresults
+convert.addEventListener("click", getResults);
+
+// Function getresults
+function getResults() {
+	fetch(`${api}`)
+		.then(currency => {
+			return currency.json();
+		}).then(displayResults);
+}
+
+// Display results after conversion
+function displayResults(currency) {
+	let fromRate = currency.rates[resultFrom];
+	let toRate = currency.rates[resultTo];
+	finalValue.innerHTML =
+		((toRate / fromRate) * searchValue).toFixed(2);
+	finalAmount.style.display = "block";
+}
+
+// When user click on reset button
+function clearVal() {
+	window.location.reload();
+	document.getElementsByClassName("finalValue").innerHTML = "";
+};
